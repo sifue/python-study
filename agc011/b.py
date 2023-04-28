154d87be2576d9e7dbb4646fc1f2ec38775ec240
Soichiro Yoshimura <yoshimura@soichiro.org>
2023-04-28 21:16:14 +0900

    ソートだけで計算する方法に改善

--- agc011/b.py
+++ agc011/b.py
@@ -1,24 +1,17 @@
 import numpy as np
-N = int, input()
+N = int(input())
 creatures = list(map(int, input().split()))
+creatures.sort()
+
 single_winner_count = 0
+current_size = 0
+for i in range(N - 1):
+    current_size += creatures[i]
+    if (creatures[i + 1] <= (current_size * 2)):
+        single_winner_count += 1
+    else:
+        single_winner_count = 0
 
-# その生き物が最後まで吸収し切れるかチェックしていく
-for i, creature in enumerate(creatures):
-    current_size = creature
-    target_creatures = np.array(creatures)
-    target_creatures = np.delete(target_creatures, i) # 自分を削除
-    target_creatures = np.sort(target_creatures) # 小さいやつから吸収していく
-
-    while (np.size(target_creatures) > 0):
-        target = target_creatures[0]
-        if (target <= (current_size * 2)): # もし吸収できるなら
-            current_size += target
-            target_creatures = np.delete(target_creatures, 0)
-        else:
-            break # これ以上吸収できないなら終了
-
-    if (np.size(target_creatures) == 0): # 対象を全部吸収できていたら
-        single_winner_count += 1 #色をカウント
+single_winner_count += 1 # 最大数のものは必ず生き残るので足す1
 
 print(single_winner_count)
